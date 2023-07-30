31f67fb87fa0f3e821a220742527f23143d87e4c
Lặc <expertdicer@gmail.com>
2023-07-30 16:19:16 +0700

    test

--- relayer/client.go
+++ relayer/client.go
@@ -475,7 +475,7 @@ func findMatchingClient(ctx context.Context, src, dst *Chain, newClientState ibc
 		clientsResp, err = src.ChainProvider.QueryClients(ctx)
 		src.log.Info(
 			"For fun",
-			zap.Any("clientsResp", clientsResp[0].ClientId),
+			zap.Any("clientsResp", clientsResp.Len()),
 		)
 		if err != nil {
 			return err
